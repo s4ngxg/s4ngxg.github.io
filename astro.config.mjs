478d5a4a83c4ca4968286cf5e85d436396e4e82b
s4ngxg <94793603+s4ngxg@users.noreply.github.com>
2026-08-22 10:12:16 +0700

    Add site URL and format icon integration list

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,22 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 
 export default defineConfig({
+  site: "https://s4ngxg.github.io",
   output: "static",
   trailingSlash: "always",
+
   integrations: [
     icon({
       include: {
         "fa6-brands": ["github", "telegram", "facebook"],
-        "fa6-solid": ["envelope", "house", "box-archive", "address-card", "folder-open", "tags"],
+        "fa6-solid": [
+          "envelope",
+          "house",
+          "box-archive",
+          "address-card",
+          "folder-open",
+          "tags",
+        ],
       },
     }),
   ],
